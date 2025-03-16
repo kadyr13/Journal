c57ec9e70931b5eddd34c6e8b667c397c6d7a5cd
kadyr13 <123488267+kadyr13@users.noreply.github.com>
2025-03-16 18:08:36 +0500

    v8

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
 // JavaScript code will go here
+$(document).ready(function () {
+    $('nav a').on('click', function (e) {
+        e.preventDefault();
+        var url = $(this).attr('href');
+
+        $.ajax({
+            url: url,
+            success: function (data) {
+                var newContent = $(data).find('#content').html();
+                $('#content').html(newContent);
+                history.pushState(null, '', url);
+            }
+        });
+    });
+
+    window.onpopstate = function () {
+        var url = location.pathname;
+
+        $.ajax({
+            url: url,
+            success: function (data) {
+                var newContent = $(data).find('#content').html();
+                $('#content').html(newContent);
+            }
+        });
+    };
+});
